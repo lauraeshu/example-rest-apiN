9f935d8b455f2642ab6f23eb6be8c081f6fb0211
Daniel Saavedra <danielink28@gmail.com>
2022-09-04 17:19:47 -0500

    Fix working lombok project

--- src/main/java/co/edu/unisabana/usuario/dto/UserDTO.java
+++ src/main/java/co/edu/unisabana/usuario/dto/UserDTO.java
@@ -4,38 +4,11 @@ import lombok.*;
 
 //Explicar lombok
 @Data
+@AllArgsConstructor
 public class UserDTO {
-    String name;
-    String lastName;
-    int age;
 
-    public UserDTO(String name, String lastName, int age) {
-        this.name = name;
-        this.lastName = lastName;
-        this.age = age;
-    }
+  private String name;
+  private String lastName;
+  private int age;
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
 }
